2ab5a9de379083385eb1c37f7a9f8e1e604e7097
y4shiro <y4shironao@gmail.com>
2021-12-19 17:18:25 +0900

    Header.jsx スタイルなど追加

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,29 @@
+import CoverImage from '../images/cover-image.jpg';
+import ProfileImage from '../images/profile-image.png';
+
 export const Header = () => {
-  const componentName = 'Header';
   return (
-    <div className="test-wrapper">
-      このコンポーネントの名前は{componentName}です
-    </div>
+    <header
+      className="main-cover"
+      style={{ backgroundImage: `url(${CoverImage})` }}
+    >
+      <div className="overlay"></div>
+      <div className="container">
+        <div className="display-table">
+          <div className="display-table-contents">
+            <div
+              className="profile-thumb"
+              style={{ backgroundImage: `url(${ProfileImage})` }}
+            ></div>
+            <h1 className="title-text">y4shiro</h1>
+            <h3 className="title-text">Engineer</h3>
+            <ul className="social-icons">
+              <li className="icon-link">{/* TODO */}</li>
+              <li className="icon-link">{/* TODO */}</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </header>
   );
 };
