5ec1e4fc1f5f1769d42f17f09748cf21477e13c9
y4shiro <y4shironao@gmail.com>
2021-12-19 21:00:54 +0900

    GitHub のユーザ情報からプログラミング言語の情報を取得してリスト化

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,4 +1,32 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 export const Skills = () => {
+  const [languageList, setLanguageList] = useState(null);
+  console.log(languageList);
+
+  useEffect(() => {
+    axios.get('https://api.github.com/users/y4shiro/repos').then((response) => {
+      const languageList = response.data.map((res) => res.language);
+      const countedLanguageList = generateLanguageCountObj(languageList);
+      setLanguageList(countedLanguageList);
+    });
+  }, []);
+
+  const generateLanguageCountObj = (allLanguageList) => {
+    const notNullLanguageList = allLanguageList.filter(
+      (language) => language != null
+    );
+    const uniqueLanguageList = [...new Set(notNullLanguageList)];
+
+    return uniqueLanguageList.map((item) => {
+      return {
+        language: item,
+        count: allLanguageList.filter((language) => language === item).length,
+      };
+    });
+  };
+
   return (
     <div id="skills">
       <div className="container">
