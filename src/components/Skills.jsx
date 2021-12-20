17ba624370d6ba54d0e31c10f23005163f8ad249
y4shiro <y4shironao@gmail.com>
2021-12-20 10:22:44 +0900

    GitHub API へアクセス時、ローディング中とエラー時の表示を実装

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,6 +5,7 @@ import {
   initialState,
   actionTypes,
 } from '../reducers/skillReducer';
+import { requestStates } from '../constants';
 
 export const Skills = () => {
   const [state, dispatch] = useReducer(skillReducer, initialState);
@@ -46,7 +47,14 @@ export const Skills = () => {
         <div className="heading">
           <h2>Skills</h2>
         </div>
-        <div className="skills-container"></div>
+        <div className="skills-container">
+          {state.requestState === requestStates.loading && (
+            <p className="description">取得中...</p>
+          )}
+          {state.requestState === requestStates.error && (
+            <p className="description">エラーが発生しました</p>
+          )}
+        </div>
       </div>
     </div>
   );
